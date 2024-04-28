2fcb8bbfb9a6ae461de1b98b7f9ee5a0fccf565e
Marian Abuhazi <ma4107@columbia.edu>
2024-04-28 13:19:06 -0400

    Testing uart works with pipeline.

--- utils/scripts/actions-pipeline/run_esp-config.sh
+++ utils/scripts/actions-pipeline/run_esp-config.sh
@@ -8,6 +8,7 @@ for accelerator in "${!latest_versions[@]}"; do
     accelerator_upper=$(echo "$accelerator" | tr '[:lower:]' '[:upper:]')
     esp_config="$HOME/esp/socs/xilinx-vc707-xc7vx485t/socgen/esp/.esp_config"
 	vivado_syn="$HOME/esp/utils/scripts/actions-pipeline/vivado_syn_$accelerator.log"
+	minicom="$HOME/esp/utils/scripts/actions-pipeline/minicom_$accelerator.log"
     cp "$defconfig" "$esp_config"
 	
     sed -i "s/CONFIG_DSU_IP = C0A80107/CONGIG_DSU_IP = C0A8011C/" "$esp_config"
@@ -16,30 +17,29 @@ for accelerator in "${!latest_versions[@]}"; do
     sed -i "s/POWER_1_0 = empty 0 0 0 0 0 0 0 0 0 0 0 0/POWER_1_0 = $accelerator_upper 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0/" "$esp_config"
 	
 	cd "$HOME/esp/socs/xilinx-vc707-xc7vx485t"
-	rm top.bit
-	rm -r vivado
-	make clean
-	cat $esp_config
-	make esp-config
-	make vivado-syn &> "$vivado_syn"
+	# rm top.bit
+	# rm -r vivado
+	# make clean
+	# make esp-config
+	# make vivado-syn &> "$vivado_syn"
+	echo "make vivado-syn"
 	if [ -s "top.bit" ]; then
-		make soft
-		make fpga-program &> "$fpga_program"
-		if grep -q ERROR "$fpga_program"; then
-        	echo "FPGA PROGRAM FAILED: $accelerator"
-		else
-			echo "FPGA PROGRAM SUCCESS: $accelerator"
+		# make soft
+		# make fpga-program &> "$fpga_program"
+		# if grep -q ERROR "$fpga_program"; then
+        # 	echo "FPGA PROGRAM FAILED: $accelerator"
+		# else
+		# 	echo "FPGA PROGRAM SUCCESS: $accelerator"
 			
-		fi
-		rm "$fpga_program"
+		# fi
+		echo "make soft and fpga-program"
+		# rm "$fpga_program"
+		make uart &
+        uart_pid=$!
+        make fpga-run
+        kill -9 "$uart_pid"
+		mv minicom.log "$minicom"
 	else
 		echo "Bitstream generation failed: $accelerator"
 	fi
-    # Clean up
-    # fi
-	# delete vivado folder and bit file
-	# make clean
-	# if (top.bit exists, make vivado-syn)
-	# make fpga-program
-	# make uart
 done
